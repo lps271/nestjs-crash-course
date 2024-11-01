e5a0de2deb0bb72476f1821349d32be443f504cf
Lucas Pereira <lpslucas27@gmail.com>
2024-11-01 19:25:17 -0300

    Created search logic by ID and creation of stock entry

--- nestjs-api-rest/src/stock-inputs/stock-inputs.service.ts
+++ nestjs-api-rest/src/stock-inputs/stock-inputs.service.ts
@@ -18,21 +18,47 @@ export class StockInputsService {
       throw new NotFoundError('Product not found');
     }
 
-    return await this.prismaService.stockInput.create({
-      data: {
-        productId: createStockInputDto.product_id,
-        quantity: createStockInputDto.quantity,
-        date: new Date(createStockInputDto.date)
-      }
-    })
+    /*
+    Abaixo nós garantimos a atomicidade das operacões (uma das propriedades ACID)
+    O objetivo é garantir que, apenas se as duas operacões ocorrerem com sucesso a alteracão no banco de dados será feita
+    Caso contrario, nenhuma é feita e o banco volta a seu estado inicial (antes da transacão)
+    */
+    const result = await this.prismaService.$transaction([
+      this.prismaService.stockInput.create({
+        data: {
+          productId: createStockInputDto.product_id,
+          quantity: createStockInputDto.quantity,
+          date: new Date(createStockInputDto.date)
+        },
+      }),
+      this.prismaService.product.update({
+        where: { id: createStockInputDto.product_id },
+        data: {
+          quantity: {
+            increment: createStockInputDto.quantity,
+          }
+        }
+      })
+    ])
+
+    return result[0];
   }
 
   findAll() {
-    return `This action returns all stockInputs`;
+    return this.prismaService.stockInput.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} stockInput`;
+  async findOne(id: number) {
+    try {
+      return await this.prismaService.stockInput.findUniqueOrThrow({
+        where: { id },
+      });
+    } catch (error) {
+      if(error.code === 'P2025') {
+        throw new NotFoundError(`Stock Input whit ID ${id} not found`);
+      }
+      throw error;
+    };
   }
 
 }
